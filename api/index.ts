438037af1c7fe7c984d5646d55b654688c1be33a
Eric <ericjiang97@gmail.com>
2020-09-20 11:50:01 +1000

    og image now fetched from title tag!!

--- api/index.ts
+++ api/index.ts
@@ -1,30 +1,53 @@
-import { IncomingMessage, ServerResponse } from 'http';
-import { parseRequest } from './_lib/parser';
-import { getScreenshot } from './_lib/chromium';
-import { getHtml } from './_lib/template';
+import { IncomingMessage, ServerResponse } from "http";
+import { parseRequest } from "./_lib/parser";
+import { getScreenshot } from "./_lib/chromium";
+import { getHtml } from "./_lib/template";
+const fetch = require("node-fetch");
 
 const isDev = !process.env.AWS_REGION;
-const isHtmlDebug = process.env.OG_HTML_DEBUG === '1';
+const isHtmlDebug = process.env.OG_HTML_DEBUG === "1";
 
-export default async function handler(req: IncomingMessage, res: ServerResponse) {
-    try {
-        const parsedReq = parseRequest(req);
-        const html = getHtml(parsedReq);
-        if (isHtmlDebug) {
-            res.setHeader('Content-Type', 'text/html');
-            res.end(html);
-            return;
-        }
-        const { fileType } = parsedReq;
-        const file = await getScreenshot(html, fileType, isDev);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', `image/${fileType}`);
-        res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
-        res.end(file);
-    } catch (e) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>Internal Error</h1><p>Sorry, there was a problem</p>');
-        console.error(e);
+export default async function handler(
+  req: IncomingMessage,
+  res: ServerResponse
+) {
+  const { url } = req;
+  if (!url) {
+    res.setHeader("Content-Type", "text/html");
+    return;
+  }
+  const [path, query] = url.split("?");
+  const pageInfo = await fetch(`https://ericjiang.dev${path}`);
+  if (!pageInfo.ok) {
+    res.setHeader("Content-Type", "text/html");
+    res.statusCode = pageInfo.status;
+    res.end();
+    return;
+  }
+  const body = await pageInfo.text();
+  const title = body.split("<title>")[1].split("</title>")[0];
+  req.url = `/${title}${query ? `?${query}` : ""}`;
+  try {
+    const parsedReq = parseRequest(req);
+    const html = getHtml(parsedReq);
+    if (isHtmlDebug) {
+      res.setHeader("Content-Type", "text/html");
+      res.end(html);
+      return;
     }
+    const { fileType } = parsedReq;
+    const file = await getScreenshot(html, fileType, isDev);
+    res.statusCode = 200;
+    res.setHeader("Content-Type", `image/${fileType}`);
+    res.setHeader(
+      "Cache-Control",
+      `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
+    );
+    res.end(file);
+  } catch (e) {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Internal Error</h1><p>Sorry, there was a problem</p>");
+    console.error(e);
+  }
 }
